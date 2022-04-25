afdf0a5a89f38bceeac59624a05ae49ed766874b
victoria-parker <victoriaparkerb@gmail.com>
2022-04-25 18:38:47 +0100

    problem 206 solved

---
+++ solutions/206.reverse-linked-list/solution.js
@@ -0,0 +1,54 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000 */
+
+//FUNCTION
+function reverseLinkedList(head){
+    
+    let prev=null
+    
+    while(head !== null){
+
+        let next=head.next;
+        head.next=prev;
+        prev=head;
+        head=next;
+
+    }
+
+    return prev
+}
+
+//TEST
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null
+    }
+}
+
+const a= new Node('A')
+const b= new Node('B')
+const c= new Node('C')
+
+a.next=b
+b.next=c
+
+
+console.log(reverseLinkedList(a))
+
