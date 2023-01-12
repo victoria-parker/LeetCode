dcc37bd51911ec8ce79a70f0ad15284d4f648ead
victoria-parker <victoriaparkerb@gmail.com>
2023-01-12 16:39:59 +0100

    557 added

---
+++ solutions/557.reverse-words-in-a-string-III.js
@@ -0,0 +1,43 @@
+/* 557. Reverse Words in a String III
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+*/
+
+
+
+// var reverseWords = function(s) {
+    
+//     let words=s.split(' ')
+
+//     //helper
+//     function reverse(w){
+//       let result=''
+      
+//       for(let i=w.length-1; i >= 0;i--){
+//           result=result+w[i]
+//       }
+//       return result
+//     }
+
+//     //with map it transforms each word using the helper
+//     return words.map(word=>reverse(word)).join(' ')
+// };
+
